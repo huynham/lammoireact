7554037ca3a8e65da754247ca3c33f4a53efc38f
huynham <91304084+huynham@users.noreply.github.com>
2021-11-01 22:14:08 +0700

    day anh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ab from './ab2.npg';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={ab} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
